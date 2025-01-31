fff3c5200228a0099d371c955dfae2e1c6caf640
Dheeraj chaubey <dheerajubecha@gmail.com>
2025-01-31 21:18:48 +0530

    Started learning python

---
+++ first.py
@@ -0,0 +1,24 @@
+# program to add two number
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+
+
+# print(a + b)
+
+# 
+
+# a = 10
+
+# b = 3
+
+# print("remainder of a/b: ", a % b)
+
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+
+# print("average: ", (a+b)/2)
+
+a = int(input("enter a number to get square:"))
+
+
+print("square of a given number", a,"is :", a*a)
