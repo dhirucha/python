c7843aa38368bb081ea541df7fc7288c889fd49a
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2025-04-22 23:08:08 +0530

    Refactor Student class to include study and play methods with corresponding print statements

--- oops/class/01_program.py
+++ oops/class/01_program.py
@@ -3,9 +3,17 @@ class Student:
         self.name = name
         self.rollno = rollno
         self.percentage = percentage
+        
+    def study(self):
+        print("I am "+ self.name +" studying")
+        
+    def play(self):
+        print("pdh liya ab khelna hai")
 
 
 s1 = Student("Dhiru", 43, 90)
+s1.study()
+s1.play()
 
 print(s1.percentage)
         
