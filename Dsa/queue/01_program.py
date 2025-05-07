8ee954ccb75e94797a3b182811803476b0ddf3f9
Dheeraj_Chaubey <dheerajubecha@gmail.com>
2025-05-07 23:25:04 +0530

    Add initial implementation of ICICI_prudential_mutual_stock with insert and pop operations

---
+++ Dsa/queue/01_program.py
@@ -0,0 +1,12 @@
+ICICI_prudential_mutual_stock = []
+
+ICICI_prudential_mutual_stock.insert(0,12)
+ICICI_prudential_mutual_stock.insert(0,11)
+ICICI_prudential_mutual_stock.insert(0,10)
+
+
+print(ICICI_prudential_mutual_stock)
+
+ICICI_prudential_mutual_stock.pop()
+
+print(ICICI_prudential_mutual_stock)
